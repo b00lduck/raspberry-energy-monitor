ae3442973e507d5812f1ff9d8369afebf6acccad
Daniel Zerlett <daniel@zerlett.eu>
2015-07-14 23:23:41 +0200

    VIEWFB: improvements memory leak fixed gitignore

---
+++ development-tools/viewfb/src/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define FRAMEBUFFER_XRES 320
+#define FRAMEBUFFER_YRES 240
+#define FRAMEBUFFER_PIXEL (FRAMEBUFFER_XRES * FRAMEBUFFER_YRES)
+#define FRAMEBUFFER_BYTES_PER_PIXEL 2 // 16-bit
+
+#define WINDOW_REFRESH_RATE 20 // Window redraw rate in Hz
--- development-tools/viewfb/src/viewfb.c
+++ development-tools/viewfb/src/viewfb.c
@@ -4,8 +4,7 @@
  *
  * Display contents of virtual framebuffer device in x window
  *
- * copyleft Daniel Zerlett 2015
- * daniel@zerlett.eu
+ * @author Daniel Zerlett
  *
  **/
 
@@ -16,49 +15,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
 
 #include "framebuffer.h"
 #include "touchscreen.h"
+#include "config.h"
 
 Display *display;
 Window win;
 static char *appname;
 
-void* tp (void* q) {
-    XEvent exppp;
-    while (1) {
-        usleep (25000);
-        memset(&exppp, 0, sizeof(exppp));
-        exppp.type = Expose;
-        exppp.xexpose.window = win;
-        XSendEvent(display,win,False,ExposureMask,&exppp);
+bool endExposeEventThread = false;
+
+/**
+ * Periodically generates an XExpose event to trigger the redraw of the screen.
+ *
+ * Runs in own thread until endExposeEventThread is true.
+ */
+void *tp (void* q) {
+    XEvent exposeEvent;
+    memset(&exposeEvent, 0, sizeof(exposeEvent));
+    exposeEvent.type = Expose;
+    exposeEvent.xexpose.window = win;
+
+    while (!endExposeEventThread) {
+        XSendEvent(display, win, False, ExposureMask, &exposeEvent);
         XFlush(display);
+        usleep (1000000 / WINDOW_REFRESH_RATE);
     }
+
     return NULL;
 }
 
+/**
+ * Create an X11 image from the pixel data in the framebuffer bitmap.
+ * Do an B5-G6-R5 color conversion to R16-G16-B16
+ */
 XImage* createImageFromFb(Display *display, Visual *visual) {
-
-    const unsigned int width = 320;
-    const unsigned int height = 240;
 
     short int* fb = fb_getBitmap();
 
-    int i, j;
-    unsigned char* image32 = (unsigned char *) malloc(320*240*4);
-    unsigned char* p = image32;
-    for(i = 0; i < width; i++) {
-        for(j=0; j < height; j++) {
-            *p++ = (*fb++ & 0b0000000000011111) << 3;
-            *p++ = (*fb & 0b0000011111100000) >> 3;
-            *p++ = (*fb & 0b1111100000000000) >> 8;
-            p++;
-        }
+    char *image32 = malloc(FRAMEBUFFER_PIXEL * 4);
+    char *p = image32;
+
+    unsigned int counter = FRAMEBUFFER_PIXEL;
+
+    while (counter--) {
+        *p++ = (*fb & 0b0000000000011111) << 3;
+        *p++ = (*fb & 0b0000011111100000) >> 3;
+        *p++ = (*fb++ & 0b1111100000000000) >> 8;
+        p++;
     }
-    return XCreateImage(display, visual, 24, ZPixmap, 0, image32, width, height, 32, 0);
+    return XCreateImage(display, visual, 24, ZPixmap, 0, image32, FRAMEBUFFER_XRES, FRAMEBUFFER_YRES, 32, 0);
 }
 
 int main(int argc, char * argv[]) {
+
 
     int rc = fb_init("/dev/fb2");
 
@@ -72,7 +85,8 @@ int main(int argc, char * argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    unsigned int border_width = 0;
+    printf("creating window\n");
+
     char *window_name = "Viewing virtual framebuffer";
     char *icon_name = "viewfb";
 
@@ -82,7 +96,6 @@ int main(int argc, char * argv[]) {
     XWMHints *wm_hints;
     XClassHint *class_hints;
     XTextProperty windowName, iconName;
-    XEvent report;
     XGCValues values;
 
     appname = argv[0];
@@ -98,12 +111,10 @@ int main(int argc, char * argv[]) {
     }
 
     int screen_num = DefaultScreen(display);
-    unsigned int width = 320;
-    unsigned int height = 240;
-    int x = DisplayWidth(display, screen_num) - width;
+    int x = DisplayWidth(display, screen_num) - FRAMEBUFFER_XRES;
     int y = 30;
 
-    win = XCreateSimpleWindow(display, RootWindow(display, screen_num), x, y, width, height, border_width, 
+    win = XCreateSimpleWindow(display, RootWindow(display, screen_num), x, y, FRAMEBUFFER_XRES, FRAMEBUFFER_YRES, 0,
         BlackPixel(display, screen_num), WhitePixel(display, screen_num));
 
     if ( XStringListToTextProperty(&window_name, 1, &windowName) == 0 ) {
@@ -116,9 +127,9 @@ int main(int argc, char * argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    size_hints->flags       = PPosition | PSize | PMinSize;
-    size_hints->min_width   = 320;
-    size_hints->min_height  = 240;
+    size_hints->flags  = PPosition | PSize | PMinSize;
+    size_hints->min_width = FRAMEBUFFER_XRES;
+    size_hints->min_height = FRAMEBUFFER_YRES;
 
     wm_hints->flags         = StateHint | InputHint;
     wm_hints->initial_state = NormalState;
@@ -140,6 +151,8 @@ int main(int argc, char * argv[]) {
     pthread_t thread;
     pthread_create(&thread, NULL, tp, NULL);
 
+    printf("entering event loop");
+
     while ( 1 ) {
         XEvent report;
         XNextEvent(display, &report);
@@ -149,7 +162,8 @@ int main(int argc, char * argv[]) {
             case Expose:
                 if ( report.xexpose.count != 0 ) break;
                 XImage* ximage = createImageFromFb(display, visual);
-                XPutImage(display, win, DefaultGC(display, 0), ximage, 0, 0, 0, 0, 320, 240);
+                XPutImage(display, win, DefaultGC(display, 0), ximage, 0, 0, 0, 0, FRAMEBUFFER_XRES, FRAMEBUFFER_YRES);
+                XDestroyImage(ximage);
                 break;
 
             case ButtonPress:
@@ -161,9 +175,14 @@ int main(int argc, char * argv[]) {
                 break;
 
             case KeyPress:
+                // cleanup and close
+                endExposeEventThread = true;
+                void* retval;
+                pthread_join(thread, &retval);
                 XFreeGC(display, gc);
                 XCloseDisplay(display);
                 fb_uninit();
+                ts_uninit();
                 exit(EXIT_SUCCESS);
  
         }
