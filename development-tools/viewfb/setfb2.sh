23f8ca902c3362350f85615caa750563f16c653b
Daniel Zerlett <daniel@zerlett.eu>
2015-07-20 17:04:18 +0200

    VIEWFB: deleted setvb2.sh

--- development-tools/viewfb/setfb2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo fbset -fb /dev/fb2 -xres 320 -yres 240 -vxres 320 -vyres 240 -depth 16 -pixclock 260415 -left 0 -right 0 -upper 0 -lower 0 -hslen 0 -vslen 0
-sudo fbset -fb /dev/fb2
