fa0db064f65328b1094929b906cce0b9f258c0da
daniel <daniel@zerlett.eu>
2015-07-24 21:45:28 +0200

    DATASERVICE: initial project setup

---
+++ raspberry/dataservice/src/main/java/com/b00lduck/raspberryEnergyMonitor/dataservice/Application.java
@@ -0,0 +1,16 @@
+package com.b00lduck.raspberryEnergyMonitor.dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Daniel Zerlett (daniel@zerlett.eu)
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
