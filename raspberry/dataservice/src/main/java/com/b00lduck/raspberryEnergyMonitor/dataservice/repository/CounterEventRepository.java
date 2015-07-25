2b476a33000c4261a2373e85ea4daba2a4026737
daniel <daniel@zerlett.eu>
2015-07-25 14:06:02 +0200

    DATASERVICE:  time range query preparations

--- raspberry/dataservice/src/main/java/com/b00lduck/raspberryEnergyMonitor/dataservice/repository/CounterEventRepository.java
+++ raspberry/dataservice/src/main/java/com/b00lduck/raspberryEnergyMonitor/dataservice/repository/CounterEventRepository.java
@@ -2,12 +2,20 @@ package com.b00lduck.raspberryEnergyMonitor.dataservice.repository;
 
 import com.b00lduck.raspberryEnergyMonitor.dataservice.entity.Counter;
 import com.b00lduck.raspberryEnergyMonitor.dataservice.entity.CounterEvent;
+import org.joda.time.DateTime;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * @author Daniel Zerlett (daniel@zerlett.eu)
  * created 24.07.2015
  */
 public interface CounterEventRepository extends CrudRepository<CounterEvent, Long> {
+
+	//@Query("select c from CounterEvent")
+	//List<CounterEvent> findTimeRange(DateTime startDate, DateTime endDate);
+
 
 }
