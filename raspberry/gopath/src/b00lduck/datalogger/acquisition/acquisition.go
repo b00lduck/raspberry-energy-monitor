d19a50d53199b33386718e3071fd777a4a554e62
Daniel Zerlett <daniel@zerlett.eu>
2015-08-15 23:16:12 +0200

    ACQUISITION: stub

--- raspberry/gopath/src/b00lduck/datalogger/acquisition/acquisition.go
+++ raspberry/gopath/src/b00lduck/datalogger/acquisition/acquisition.go
@@ -11,33 +11,44 @@ import (
 func main() {
 
     if err := embd.InitGPIO(); err != nil {
-	panic(err)
+		panic(err)
     }
     defer embd.CloseGPIO()
 
     led, err := embd.NewDigitalPin(17)
     if err != nil {
-	panic(err)
+		panic(err)
     }
     defer led.Close()
 
     if err := led.SetDirection(embd.In); err != nil {
-	panic(err)
+		panic(err)
     }
 
-//    if err := led.PullUp(); err != nil {
-//	panic(err)
-//    }
+	state := false
+	count := 0
 
     for {
-	
         pin,err := led.Read()
-	if  err != nil {
-    	    panic(err)
-	}
-	fmt.Println(pin)
+		if  err != nil {
+			panic(err)
+		}
 
-	time.Sleep(1 * time.Second)
+		if pin == true {
+			if state == false {
+				count++
+			}
+		} else {
+			count = 0
+			state = false
+		}
+
+		if count >= 3 {
+			state = true
+			fmt.Println("COUNT")
+		}
+
+		time.Sleep(10 * time.Millisecond)
 
     }
 }
